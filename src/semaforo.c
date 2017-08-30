4d7b491aa2293bd98b0ef48305ac97cbab2f561a
jcarlos <carlosmaster10@gmail.com>
2017-08-30 00:35:55 -0300

    commented code

--- src/semaforo.c
+++ src/semaforo.c
@@ -1,11 +1,14 @@
 //---------------------------------------------------------------------------
+
 #ifdef _WIN32
 
+// WINDOWS
 #include <windows.h>
 #include <gl\glut.h>
 
 #else
 
+// LINUX
 #include <GL/glut.h>
 #include <unistd.h>
 
@@ -15,18 +18,27 @@
 //---------------------------------------------------------------------------
 #include "semaforo.h"
 
+// variaveis de controle de rotacao
 float RotX = 0.0, RotY = 0.0, RotZ = 0.0;
+// controla se ativa a visualizacao do sistema de coordenadas
 int verSistCoord = 0;
-// window dim
+
+// Dimensiona a projecao
 int nearV = -15;
 int farV = 15;
 int leftV = -10;
 int rightV = 10;
 int upperV = 10;
 int bottomV = -10;
+
+// temporizador no semaforo
 unsigned int time;
 
 //---------------------------------------------------------------------------
+
+/**
+ * dimensiona janela
+ */
 void dimensionaJanela(int w, int h) {
 	glViewport(0, 0, w, h);
 	glMatrixMode(GL_PROJECTION);
@@ -35,31 +47,39 @@ void dimensionaJanela(int w, int h) {
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 }
+
+/**
+ * limpa-buffers e outras inicializacoes;
+ */
 void initGL() {
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 	glEnable(GL_DEPTH_TEST);
-	//limpa-buffers e outras inicializacoes;
 }
 
-
-
-
+/**
+ * desenha semaforo
+ */
 void desenha() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glPushMatrix();
+	// rotaciona
 	glRotatef(RotX, 1.0, 0.0, 0.0);
 	glRotatef(RotY, 0.0, 1.0, 0.0);
 	glRotatef(RotZ, 0.0, 0.0, 1.0);
 
+	// desenha sistema de coordenadas se ativo
 	if (verSistCoord)
 		sistCoord();
 
+	// desenha semaforo
 	semaforo();
 
 	glPopMatrix();
 	glutSwapBuffers();
 	
 }
+
+// teclas padrao
 void teclas(unsigned char key, int x, int y) {
 	switch (key) {
 	case 27:
@@ -68,6 +88,7 @@ void teclas(unsigned char key, int x, int y) {
 		//...
 	}
 }
+
 // Trata das teclas especiais
 void teclas_especiais(int key, int x, int y) {
 	switch (key) {
@@ -102,15 +123,21 @@ void teclas_especiais(int key, int x, int y) {
 	glutPostRedisplay();
 }
 
+/**
+ * animacao
+ */
 void animation () {
 	
+	// pega o tempo atual
 	unsigned int newTime = glutGet(GLUT_ELAPSED_TIME);
 
 	if (newTime - time < 1000)
 		return;
 
+	// atualiza tempo
 	time = newTime;
-		
+
+	// alterna entre os circulos do semaforo
 	if (redOn + yellowOn + greenOn == 0 || yellowOn == 1) {
 		yellowOn = 0;
 		redOn = 1;
@@ -130,25 +157,29 @@ void animation () {
 
 int main(int argc, char *argv[]) {
 		
-	puts("setas-gira, F1-mostra sist.coord., ESC-encerra");
+	puts("setas - gira (eixo x/y), pageup/pagedown - gira (eixo z), F1-mostra sist.coord., ESC-encerra");
 
+	// prepara
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
 	glutInitWindowSize(500, 600);
 
 	glutCreateWindow("Semaforo");
 
+	// seta as funcoes
 	glutReshapeFunc(dimensionaJanela);
 	glutDisplayFunc(desenha);
 	glutKeyboardFunc(teclas);
 	glutSpecialFunc(teclas_especiais);
+	// pega tempo atual
 	time = glutGet(GLUT_ELAPSED_TIME);
 
-	// chama a func animation qd o glut n ta tratando os eventos
+	// chama a funcao animation quando o glut nao ta tratando os eventos
 	glutIdleFunc( animation );
 
-	
+	// inicia tudo
 	initGL();
 	glutMainLoop();
+
 	return (0);
 }
--- src/semaforo.h
+++ src/semaforo.h
@@ -1,10 +1,14 @@
 #include <math.h>
 #define M_PI 3.14159265358979323846
 
+// variaveis de controle do semaforo
 int redOn = 0;
 int yellowOn = 0;
 int greenOn = 0;
-/*Lista de vertices:  */
+
+/*Lista de vertices ("pontos") dos poligonos:
+ * cada vertice tem coordenadas x, y e z
+ * */
 GLfloat vert[][3]=
 {
 	// vertices do topo
@@ -26,9 +30,15 @@ GLfloat vert[][3]=
 	{ 0.3, -8.0,  0.0},
 	{ 0.0, -8.0, -0.3}
 };
-/* Lista de poligonos:   */
+
+/* Lista de poligonos
+ * o primeiro numero: a quantia de vertices do poligono (4 significa 4 vertices)
+ * os outros numeros sao os vertices da lista de vertices
+ * (por exemplo, 0 significa o vertice 0 da lista de vertices)
+ * */
 GLint polig[][5] =
 {
+	// topo
     {4, 0, 1, 2, 3},
     {4, 0, 7, 6, 1},
     {4, 1, 6, 5, 2},
@@ -43,67 +53,105 @@ GLint polig[][5] =
 	{4, 11, 8, 15, 12},
 	{4, 12, 13, 14, 15},
 };
+
+/**
+ * desenha cada poligono
+ * recebe um array, que representa um elemento da lista de poligonos
+ */
 void Poligono(GLint *p)
 {
     int i;
+    // comeca a desenhar em linha em loop
     glBegin(GL_LINE_LOOP);
+    // constroi o poligono pegando qual o vertice do array, e esse vertice da lista de vertices
     for(i=1; i<=p[0]; i++)
         glVertex3fv(vert[p[i]]);
     glEnd();
 }
 
+/**
+ * desenha cada circulo com preenchimento
+ * red, green, blue sao as cores
+ * x, y, z: a posicao do circulo
+ * radius: raio do circulo
+ */
 void circulo(GLfloat red, GLfloat green, GLfloat blue, GLfloat x, GLfloat y,
 		GLfloat z, GLfloat radius) {
 
+	// cor
 	glColor3f(red,green,blue);
+	// objeto com preenchimento
 	glBegin(GL_TRIANGLE_FAN);
 	int i;
 
-        for(i=0;i<100;i++)
-            glVertex3d(x + (radius * cos(i * 2.0 * M_PI / 100)) ,
-                    y + (radius * sin(i * 2.0 * M_PI / 100)),
-                    z - (radius * cos(i * 2.0 * M_PI / 100)));
+	// desenha o circulo
+	for(i=0;i<100;i++)
+		glVertex3d(x + (radius * cos(i * 2.0 * M_PI / 100)) ,
+				y + (radius * sin(i * 2.0 * M_PI / 100)),
+				z - (radius * cos(i * 2.0 * M_PI / 100)));
 	glEnd();
 }
 
-
-
-
+/**
+ * desenha o semaforo
+ */
 void semaforo(void)
 {
     int i;
-    glColor3f(1.0,1.0,0.5);      // todas as linhas em amarelo
-    for(i=0; i<12; i++) Poligono(polig[i]);
 
+    // todas as linhas em amarelo
+    glColor3f(1.0,1.0,0.5);
+
+    // desenha todos os poligonos da lista de poligonos
+    for(i=0; i<12; i++)
+    	Poligono(polig[i]);
+
+    // posicoes padrao e raio dos circulos
     GLfloat x = 0.5, y = 4.0 + (4.0/6.0), z = 0.5, radius = 4.0/9.0;
 
-    // semaforo verde
+    /**
+     * cada circulo pode ter mais ou menos intensidade, a depender das variaveis
+     * de controle greenOn, yellowOn e redOn
+     */
+
+    // circulo verde
     if (greenOn)
 		circulo(0.0,1.0,0.0,x,y,z,radius);
 	else 
 		circulo(0.0,0.5,0.0,x,y,z,radius);
-    // semaforo amarelo
+
+    // circulo amarelo
     if (yellowOn)
 		circulo(1.0,1.0,0.0,x,y + (4.0/3.0),z,radius);
 	else
 		circulo(0.5,0.5,0.0,x,y + (4.0/3.0),z,radius);
-    // semaforo vermelho
+    // circulo vermelho
     if (redOn)
 		circulo(1.0,0.0,0.0,x,y + (8.0/3.0),z,radius);
 	else 
 		circulo(0.3,0.0,0.0,x,y + (8.0/3.0),z,radius);
 }
+
+/**
+ * desenha o sistema de coordenadas
+ */
 void sistCoord(void)
 {
+	// comeca a desenhar cada linha
+	// cada linha so precisa de dois vertices
     glBegin(GL_LINES);
+
     glColor3f(1.0, 0.0, 0.0);  // eixo x - vermelho
     glVertex3f(-3.0, 0.0, 0.0);
     glVertex3f(3.0,  0.0, 0.0);
+
     glColor3f(0.0, 1.0, 0.0);  // eixo y - verde
     glVertex3f(0.0, -3.0, 0.0);
     glVertex3f(0.0,  3.0, 0.0);
+
     glColor3f(0.0, 0.0, 1.0);  // eixo z - azul
     glVertex3f(0.0, 0.0, -3.0);
     glVertex3f(0.0, 0.0,  3.0);
+
     glEnd();
 }
